0b61d0a331e6bbc929ba435b24175da778660f34
student <student@localhost.localdomain>
2017-09-14 16:05:19 -0600

    Lab 1 part 1

---
+++ Arch1Lab.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+main(){
+  int c;
+
+  while((c=getchar()) != EOF){
+    if(c == '\t'){
+      putchar('\t');
+    }
+    else if(c == '\b'){
+      putchar('\b');
+    }
+    else if(c == '\\'){
+      putchar('\\');
+    }
+    else{
+      putchar(c);
+    }
+  }
+
+
+
+}
