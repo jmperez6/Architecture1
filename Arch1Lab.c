eb83ce23853dbe0441b051c661b579a0d75e24aa
jmperez6 <jmperez6@miners.utep.edu>
2017-09-14 18:12:09 -0600

    Finished all of Lab 1

--- Arch1Lab.c
+++ Arch1Lab.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
 main(){
+  Exercise1_10();
+  Exercise1_11();
+  Exercise1_15();
+  Exercise1_20();
+  Exercise1_X(50.00);
+  
+
+
+}
+
+
+
+
+Exercise1_10(){
   int c;
 
   while((c=getchar()) != EOF){
@@ -18,6 +32,55 @@ main(){
     }
   }
 
+}
+
+Exercise1_11(){
+  printf("You would printf the result of the word count program with a word you know and check the output.")
 
 
+}
+
+Exercise1_15(){
+  int fahr, celsius;
+  int lower, upper, step;
+
+  lower = 0;
+  upper = 300;
+  step = 20;
+
+  fahr = lower;
+  while (fahr <= upper) {
+    celsius = ConvertToCelsius(fahr);
+    printf("%d\t%d\n", fahr, celsius);
+    fahr = fahr + step;
+  }
+}
+
+ConvertToCelsius(int fahr){
+  return 5 * (fahr-32) / 9;
+}
+
+
+Exercise1_20(){
+  int c;
+
+  while((c=getchar()) != EOF){
+    if(c == '\t'){
+      for(int i = 0; i<4; i++){ /* Assuming a tab is equal to 4 spaces*/
+	putchar(' ')
+      }
+    }
+    else{
+      putchar(c)
+    }
+  }
+}
+
+
+Exercise1_X(double C){
+  for(double i = C/5; i<C; i+=i){
+    R = (i + 273.15) * 9/5;
+    printf("%lf", i);
+    printf(" Celsius to Rankine is: %lf", R);
+  }
 }
